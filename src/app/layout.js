8d7c87e10f061e93400a810fc525d3cee550a0f8
seanhillweb <seanhillweb@gmail.com>
2023-08-09 11:11:31 -0500

    feat: initial commit and setup

---
+++ src/app/layout.js
@@ -0,0 +1,31 @@
+/**
+ * Summary.
+ *
+ * Description. A layout is UI that is shared between routes.
+ *
+ * @link https://nextjs.org/docs/app/api-reference/file-conventions/layout
+ */
+
+import "@/styles/globals.css";
+
+import Attribution from "@/components/attribution";
+
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Frontend Mentor | Notifications page",
+  description: "Built with React, Next.js, and Tailwind CSS",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {children}
+        <Attribution />
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,18 @@
+/**
+ * Summary. Frontend Mentor Challenge.
+ *
+ * Description. A notifications page, that allows to mark all messages as read, or mark invdividual message as read.
+ *
+ * @link
+ */
+
+export default function Home() {
+  return (
+    <main
+      className="flex min-h-screen flex-col items-center justify-between p-24"
+      aria-label="Content"
+    >
+      <h1 className="sr-only">Frontend Mentor, Notifications page</h1>
+    </main>
+  );
+}
