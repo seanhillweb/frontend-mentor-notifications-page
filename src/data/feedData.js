ffe027fa3b744f385300e49b80cc67da7e08256e
seanhillweb <seanhillweb@gmail.com>
2023-08-09 15:38:04 -0500

    feat: add simple feed data

---
+++ src/data/feedData.js
@@ -0,0 +1,94 @@
+export const feedData = [
+  {
+    userName: "Mark Webber",
+    userImage: {
+      src: "/avatar-mark-webber.webp",
+      alt: "Profile image of Mark Webber",
+    },
+    timeElapsed: "1m ago",
+    content: "reacted to your recent post",
+    feedLink: {
+      href: "/",
+      text: "My first tournament today!",
+    },
+    unread: true,
+  },
+  {
+    userName: "Angela Gray",
+    userImage: {
+      src: "/avatar-angela-gray.webp",
+      alt: "Profile image of Angela Gray",
+    },
+    timeElapsed: "5m ago",
+    content: "followed you",
+    unread: true,
+  },
+  {
+    userName: "Jacob Thompson",
+    userImage: {
+      src: "/avatar-jacob-thompson.webp",
+      alt: "Profile image of Jacob Thompson",
+    },
+    timeElapsed: "1 day ago",
+    content: "has joined your group",
+    feedLink: {
+      href: "/",
+      text: "Chess Club",
+    },
+    unread: true,
+  },
+  {
+    userName: "Rizky Hasanuddin",
+    userImage: {
+      src: "/avatar-rizky-hasanuddin.webp",
+      alt: "Profile image of Rizky Hasanuddin",
+    },
+    timeElapsed: "5 days ago",
+    content: "sent you a private message",
+    message:
+      "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.",
+    unread: false,
+  },
+  {
+    userName: "Kimberly Smith",
+    userImage: {
+      src: "/avatar-kimberly-smith.webp",
+      alt: "Profile image of Kimberly Smith",
+    },
+    timeElapsed: "1 week ago",
+    content: "commented on your picture",
+    feedImage: {
+      src: "/image-chess.webp",
+      alt: "Young person playing chess",
+    },
+    unread: false,
+  },
+  {
+    userName: "Nathan Peterson",
+    userImage: {
+      src: "/avatar-nathan-peterson.webp",
+      alt: "Profile image of Nathan Peterson",
+    },
+    timeElapsed: "2 weeks ago",
+    content: "reacted to your recent post",
+    feedLink: {
+      href: "/",
+      text: "5 end-game strategies to increase your win rate",
+    },
+    unread: false,
+  },
+  {
+    userName: "Anna Kim",
+    userImage: {
+      src: "/avatar-anna-kim.webp",
+      alt: "Profile image of Anna Kim",
+    },
+    timeElapsed: "2 weeks ago",
+    content: "left the group",
+    feedLink: {
+      href: "/",
+      text: "Chess Club",
+    },
+    unread: false,
+  },
+];
